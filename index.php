358f14a296221f3a5a534d7691d0b9239399a4d2
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-07-13 16:01:44 +0200

    created the html and css to show the elements from db

--- index.php
+++ index.php
@@ -1,30 +1,56 @@
-<?php 
+<?php require_once "./db.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Oop 1 Movies</title>
+</head>
+<body>
+    <div class="movie-container">
+        <h1>Movie List</h1>
 
-class Movie {
-    public $name;
-    public $genre;
-    public $discount;
+        <?php foreach ($clients as $client): ?>
+            <div>
+                <h2><?php echo $client->name; ?></h2>
+                <p>Genres: <?php echo implode(", ", $client->genres); ?></p>
+                <p>Discount: <?php echo $client->discount; ?>%</p>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</body>
+</html>
 
-    function __construct($name, $genre) {
-        $this->name = $name;
-        $this->genre = $genre;
-    }
-
-    public function discount($age) {
-        if($age < 16){
-            $this->discount = 30;
-        }else{
-            $this->discount = 0;
-        }
-    }
+<style>
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f2f2f2;
+    margin: 2rem 0;
+    padding: 0;
+    text-align: center;
 }
 
-$client1 = new Movie('Mission impossible', 'Action');
-$client1->discount(25);
-var_dump($client1);
+h1 {
+    color: #333;
+    padding: 20px 0;
+}
 
-$client2 = new Movie('Back to the future', ['Science', 'Fiction comedy']);
-$client2->discount(15);
-var_dump($client2);
+.movie-container {
+    max-width: 800px;
+    margin: 0 auto;
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
 
-?>
+.movie-container h2 {
+    color: #333;
+    margin-bottom: 5px;
+}
+
+.movie-container p {
+    color: #666;
+    margin-top: 0;
+}
+</style>
