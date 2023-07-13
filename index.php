fe5585d459ab98d953fec6466126f16ea3a2aefe
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-07-13 15:21:02 +0200

    added two instance and printed on screen the relative proprieties

--- index.php
+++ index.php
@@ -10,13 +10,21 @@ class Movie {
         $this->genre = $genre;
     }
 
-    public function discount() {
-        if($eta < 16){
+    public function discount($age) {
+        if($age < 16){
             $this->discount = 30;
         }else{
             $this->discount = 0;
         }
     }
 }
+
+$client1 = new Movie('Mission impossible', 'Action');
+$client1->discount(25);
+var_dump($client1);
+
+$client2 = new Movie('Back to the future', 'Science fiction comedy');
+$client2->discount(15);
+var_dump($client2);
 
 ?>
